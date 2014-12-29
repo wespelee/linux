5438c247f0df13b68e57156d97284cf7d57ace93
Eric Anholt <eric@anholt.net>
2014-12-28 19:46:25 -1000

    drm/vc4: Don't spew error messages when a wait is interrupted by a signal.
    This is typical in X, which uses alarms for its scheduling.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_gem.c
+++ drivers/gpu/drm/vc4/vc4_gem.c
@@ -152,7 +152,7 @@ vc4_wait_for_seqno(struct drm_device *dev, uint64_t seqno, uint64_t timeout_ns)
 
 	finish_wait(&vc4->job_wait_queue, &wait);
 
-	if (ret) {
+	if (ret && ret != -ERESTARTSYS) {
 		DRM_ERROR("timeout waiting for render thread idle\n");
 		return ret;
 	}
