9e1313995b1ce8ded070293a4b2c9bebe6553a4d
Eric Anholt <eric@anholt.net>
2015-05-15 12:44:55 -0700

    drm/vc4: Add a debugfs node for looking at BO allocation stats.
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_debugfs.c
+++ drivers/gpu/drm/vc4/vc4_debugfs.c
@@ -20,6 +20,7 @@ static const struct drm_info_list vc4_debugfs_list[] = {
 	{"v3d_regs", vc4_v3d_debugfs_regs, 0},
 	{"hdmi_regs", vc4_hdmi_debugfs_regs, 0},
 	{"hvs_regs", vc4_hvs_debugfs_regs, 0},
+	{"bo_stats", vc4_bo_stats_debugfs, 0},
 };
 #define VC4_DEBUGFS_ENTRIES ARRAY_SIZE(vc4_debugfs_list)
 
