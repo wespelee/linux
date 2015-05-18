95bb1bd536d8b1f6097ead307ea767875cf399a1
Eric Anholt <eric@anholt.net>
2015-05-18 11:20:03 -0700

    clk: bcm2835: Add binding docs for the Raspberry Pi clock provider (v3)
    The hardware clocks are not controllable by the ARM, so we have to
    make requests to the firmware to do so from the VPU side.  This will
    let us replace fixed clocks in our DT with actual clock control (and
    correct frequency information).
    
    v2: Include the dt-bindings header in this commit instead of the next
        one.  Make the clock indices match the firmware clock IDs.  Rename
        the binding's compat string.  Move the firmware phandle to be
        under a vendor-specific namespace.
    v3: Mention 'clk' in the subject instead of the more generic
        dt/bindings.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>

---
+++ include/dt-bindings/clk/raspberrypi.h
@@ -0,0 +1,23 @@
+#/*
+ *  Copyright © 2015 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_CLK_RASPBERRYPI_H
+#define _DT_BINDINGS_CLK_RASPBERRYPI_H
+
+#define RPI_CLOCK_EMMC	1
+#define RPI_CLOCK_UART0	2
+#define RPI_CLOCK_ARM	3
+#define RPI_CLOCK_CORE	4
+#define RPI_CLOCK_V3D	5
+#define RPI_CLOCK_H264	6
+#define RPI_CLOCK_ISP	7
+#define RPI_CLOCK_SDRAM	8
+#define RPI_CLOCK_PIXEL	9
+#define RPI_CLOCK_PWM	10
+
+#endif
